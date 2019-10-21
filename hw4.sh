ee87489ec019842c6c49c6a6d636a77b74f8bfae
tmoddelmog <tmoddelmog@ksu.edu>
2019-10-21 16:33:09 -0500

    quick change to script

--- hw4.sh
+++ hw4.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for i in 16 8 4 2
-do
-    ./hw4_1_working.x -np $i 50000 1000000
-    ./hw4_2_working.x -np $i 50000 1000000
-    ./hw4_3_working.x -np $i 50000 1000000
+for j in 1 2 3 4 5
+do 
+    for i in 16 8 4 2
+    do
+        ./hw4_1_working.x -np $i 50000 1000000
+        ./hw4_2_working.x -np $i 50000 1000000
+        ./hw4_3_working.x -np $i 50000 1000000
+    end
 end
