313b1bc18daa46e447aed3f8320574e0275cb9a1
tmoddelmog <tmoddelmog@ksu.edu>
2019-10-29 18:11:21 -0500

    MPI working

--- HW5/hw5.c
+++ HW5/hw5.c
@@ -1,27 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
+#include <mpi.h>
+
+
+int num_N_Global;
 
 int main(int argc, char *argv[])
 {
-    int num_N,  // number of numbers
-        sum = 0,
-        partialSum = 0,
-        mean,
+    int num_N,  // local number of numbers
+        rc,
+        numTasks,
+        rank,
+        remainder,
         i;
-    double stdDev;
+    double stdDev,
+            mean,
+            sum = 0,
+            partialSum = 0;
+    int* numbers;
+    double* receiveBuff;
+    MPI_Status status;
 
+    // Init MPI
+    rc = MPI_Init(&argc, &argv);
+    if (rc != MPI_SUCCESS)
+    {
+        printf("Error starting MPI.\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+    }
+    MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // check commandline args and then set num_N to given number
     if (argc == 2)
     {
-        num_N = atoi(argv[1]);
+        num_N_Global = atoi(argv[1]);
+    }
+    num_N = num_N_Global / numTasks;
+    
+    remainder = num_N_Global % numTasks;
+    if (rank == (numTasks - 1))
+    {
+        num_N += remainder;
     }
 
     // allocate numbers array and generate random numbers
-    int* numbers = (int *) malloc(num_N * sizeof(int));
+    receiveBuff = (double *) malloc(numTasks * sizeof(double));
+    numbers = (int *) malloc(num_N * sizeof(int));
     for (i = 0; i < num_N; i++)
     {
         numbers[i] = rand() % 100 + 1;  // generate a number between 0 and 100
-        printf("number %d: %d\n", i, numbers[i]);
+        printf("Number %d: %d\n", i, numbers[i]);
     }
 
     // caclulate mean
@@ -29,19 +60,41 @@ int main(int argc, char *argv[])
     {
         sum += numbers[i];
     }
-    mean = sum / num_N;
-    printf("Sum: %d\n", sum);
-    printf("Mean: %d\n", mean);
+    mean =  (double) sum / num_N;
+    printf("Sum %d: %f\n", rank, sum);
+    printf("Mean %d: %f\n", rank, mean);
 
-    // calculate std. dev.
+    MPI_Allgather(&mean, 1, MPI_DOUBLE, receiveBuff, 1, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    mean = 0;
+    for (i = 0; i < numTasks; i++)
+    {
+        mean += receiveBuff[i];
+    }
+    mean = mean / numTasks;
+
     for (i = 0; i < num_N; i++)
     {
-        partialSum += (numbers[i] - mean) * (numbers[i] - mean);
+       partialSum += (numbers[i] - mean) * (numbers[i] - mean);
     }
-    stdDev = (double) partialSum / (num_N - 1);
-    stdDev = sqrt(stdDev);
-    printf("stdDev: %f\n", stdDev);
 
+    MPI_Gather(&partialSum, 1, MPI_DOUBLE, receiveBuff, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {  
+        sum = 0;
+        for (i = 0; i < numTasks; i++)
+        {
+            sum += receiveBuff[i];
+        }
+        // calculate std. dev.
+        stdDev =  sum / (num_N_Global - 1);
+        stdDev = sqrt(stdDev);
+        printf("stdDev: %f\n", stdDev);
+    }
+    
     free(numbers);
+    free(receiveBuff);
+    MPI_Finalize();
     return 0;
 }
