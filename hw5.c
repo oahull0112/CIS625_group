7dd693d4e3594298d0a585013e4c9affc16b573d
tmoddelmog <tmoddelmog@ksu.edu>
2019-10-29 16:11:07 -0500

    started hw5

---
+++ hw5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int num_N,  // number of numbers
+        i;
+
+    // check args and set num_N to number given from commandline
+    if (argc == 2)
+    {
+        num_N = atoi(argv[1]);
+    }
+
+    // allocate numbers array and generate random numbers
+    int* numbers = (int*) malloc(num_N);
+    for (i = 0; i < num_N; i++)
+    {
+        numbers[i] = rand() % 100 + 1;  // generate a number between 0 and 100
+        printf("number %d: %d\n", i, numbers[i]);
+    }
+
+    free(numbers);
+    return 0;
+}
